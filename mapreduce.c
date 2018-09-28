be5bdbaad87e76806043d41a8d1a5bb335622315
Derek Sagun <ds1378@top.cs.rutgers.edu>
2018-09-28 00:55:55 -0400

    delim array

--- mapreduce.c
+++ mapreduce.c
@@ -50,9 +50,49 @@ int main(int argc, char ** argv){
     fclose(f);
 
     buffer[fsize] = 0;
+    int * delimArr = (int*)malloc(sizeof(int) * (num_maps +1));
+    
+    //loop through buffer and splits into num_maps -1, then the last thread will handle the rest
+    // if nummaps == 1 no need for delim
+    
+    int position;
+    int delimPos = 1;
+    delimArr[0] = -1;
+    
+    if(num_maps == 1){
+    	position = fsize - 1; // -1 because the size is 1 greater than the position
+    	delimArr[delimPos] = position;
+    } else { //if num_maps(# of threads) > 1
+    	int size = fsize/num_maps; // gets the ideal sizes of each thread
+    	int i;
+    	for(i = 0; i < num_maps; i++){
+    		
+    		if(i == num_maps - 1){ //this is created for the last iteration of the loop
+    			delimArr[delimPos] = fsize - 1;
+    			break;
+    		}
+    		
+    		position = ((i+1) * size) - 1; //gets the position at the (i+1)th thread    	
 
+    		while(buffer[position] != '\n'){
+    			position--;
+    		}
+//    		printf("At position %d is this char %c\n", position, buffer[position]);
+    		delimArr[delimPos] = position;
+    		delimPos++;
+    		
+    		
+    	}
+    	
+    
+    }
 
     printf("%s\n", buffer);
-
+	int i;
+	printf("Delim Arr:\n");
+	for(i = 0; i < num_maps +1; i++){
+		printf("%d\n",delimArr[i]);
+	}
+	printf("%ld\n", fsize -1);
     return 0;
 }
