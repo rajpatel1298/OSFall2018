5e602de7c4f19be6da0f16992302dcc36730770c
ya motha <33840512+LuizRivera@users.noreply.github.com>
2018-12-04 20:32:48 -0500

    basic thing

---
+++ tmpz/ssfs.c
@@ -0,0 +1,131 @@
+#define FUSE_USE_VERSION 30
+
+#include <fuse.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#define PORT 8080 
+
+int sock = 0, valread; 
+
+static int do_getattr( const char *path, struct stat *st )
+{
+	printf( "[getattr] Called\n" );
+	printf( "\tAttributes of %s requested\n", path );
+	
+	// GNU's definitions of the attributes (http://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html):
+	// 		st_uid: 	The user ID of the file’s owner.
+	//		st_gid: 	The group ID of the file.
+	//		st_atime: 	This is the last access time for the file.
+	//		st_mtime: 	This is the time of the last modification to the contents of the file.
+	//		st_mode: 	Specifies the mode of the file. This includes file type information (see Testing File Type) and the file permission bits (see Permission Bits).
+	//		st_nlink: 	The number of hard links to the file. This count keeps track of how many directories have entries for this file. If the count is ever decremented to zero, then the file itself is discarded as soon 
+	//						as no process still holds it open. Symbolic links are not counted in the total.
+	//		st_size:	This specifies the size of a regular file in bytes. For files that are really devices this field isn’t usually meaningful. For symbolic links this specifies the length of the file name the link refers to.
+	
+	st->st_uid = getuid(); // The owner of the file/directory is the user who mounted the filesystem
+	st->st_gid = getgid(); // The group of the file/directory is the same as the group of the user who mounted the filesystem
+	st->st_atime = time( NULL ); // The last "a"ccess of the file/directory is right now
+	st->st_mtime = time( NULL ); // The last "m"odification of the file/directory is right now
+	
+	if ( strcmp( path, "/" ) == 0 )
+	{
+		st->st_mode = S_IFDIR | 0755;
+		st->st_nlink = 2; // Why "two" hardlinks instead of "one"? The answer is here: http://unix.stackexchange.com/a/101536
+	}
+	else
+	{
+		st->st_mode = S_IFREG | 0644;
+		st->st_nlink = 1;
+		st->st_size = 1024;
+	}
+	
+	return 0;
+}
+
+static int do_readdir( const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi )
+{
+
+    char *hello = "Hello from client"; 
+    char buff[1024] = {0}; 
+    send(sock , hello , strlen(hello) , 0 ); 
+    printf("Hello message sent\n"); 
+    valread = read( sock , buff, 1024); 
+    printf("%s\n",buff ); 
+    filler(buffer,buff, NULL,0);
+
+
+/*
+	printf( "--> Getting The List of Files of %s\n", path );
+     //   filler( buffer, ".", NULL, 0 ); // Current Directory
+	//filler( buffer, "..", NULL, 0 ); // Parent Directory
+        filler( buffer, "file1", NULL, 0 ); // Current Directory
+	filler( buffer, "file2", NULL, 0 ); // Parent Directory
+*/
+	/*
+	filler( buffer, ".", NULL, 0 ); // Current Directory
+	filler( buffer, "..", NULL, 0 ); // Parent Directory
+	
+	if ( strcmp( path, "/" ) == 0 ) // If the user is trying to show the files/directories of the root directory show the following
+	{
+		filler( buffer, "file54", NULL, 0 );
+		filler( buffer, "file349", NULL, 0 );
+	}
+	*/
+	return 0;
+}
+
+
+
+static struct fuse_operations operations = {
+    .getattr	= do_getattr,
+    .readdir	= do_readdir
+    //.read		= do_read,
+};
+
+int main( int argc, char *argv[] )
+{
+
+struct sockaddr_in address; 
+    
+    struct sockaddr_in serv_addr; 
+    
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+    { 
+        printf("\n Socket creation error \n"); 
+        return -1; 
+    } 
+   
+    memset(&serv_addr, '0', sizeof(serv_addr)); 
+   
+    serv_addr.sin_family = AF_INET; 
+    serv_addr.sin_port = htons(PORT); 
+       
+    // Convert IPv4 and IPv6 addresses from text to binary form 
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
+    { 
+        printf("\nInvalid address/ Address not supported \n"); 
+        return -1; 
+    } 
+   
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    { 
+        printf("\nConnection Failed \n"); 
+        return -1; 
+    } 
+    
+
+	return fuse_main( argc, argv, &operations, NULL );
+}
+
+
+
+
+
+
+
